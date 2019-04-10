7208a30ab4614520b63ddd413482f410d16b6cb8
Alice Lu <lalice72098@yahoo.com>
2019-04-10 01:32:21 -0400

    config db for prod

--- db.js
+++ db.js
@@ -17,4 +17,23 @@ const User = new mongoose.Schema({
 mongoose.model("Card", Card);
 mongoose.model("User", User);
 
-mongoose.connect('mongodb://localhost/qrPortfolio', { useNewUrlParser: true });
+// is the environment variable, NODE_ENV, set to PRODUCTION? 
+let dbconf;
+if (process.env.NODE_ENV === 'PRODUCTION') {
+ // if we're in PRODUCTION mode, then read the configration from config.json
+ // use blocking file io to do this...
+ const fs = require('fs');
+ const path = require('path');
+ const fn = path.join(__dirname, 'config.json');
+ const data = fs.readFileSync(fn);
+
+ // our configuration file will be in json, so parse it and set the
+ // connection string appropriately!
+ const conf = JSON.parse(data);
+ dbconf = conf.dbconf;
+} else {
+ // if we're not in PRODUCTION mode, then use
+ dbconf = 'mongodb://localhost/qrPortfolio';
+}
+
+mongoose.connect(dbconf, { useNewUrlParser: true });
